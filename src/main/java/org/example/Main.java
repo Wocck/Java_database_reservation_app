66992057d357745fe716f800b44669c4b9ac58a5
wojte <wojteksekua@gmail.com>
2023-05-23 10:47:04 +0200

    added Jar

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -8,15 +8,6 @@ import java.sql.*;
 
 public class Main {
     public static void main(String[] args) throws SQLException {
-        MetricsModel metrics = new MetricsModel();
-        metrics.setTotalReservations(10);
-        metrics.setReservationDuration(120.5);
-        metrics.setBusiestReservationDay("2023-05-22");
-        metrics.setBusiestReservationTimeSlot(14);
-        // Populate other metric data
-
-        // Save the metric data to a JSON file
-        metrics.saveToJsonFile("metrics.json");
         LoginApp.main(args);
     }
 }
